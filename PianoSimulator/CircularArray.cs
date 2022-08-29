fc66c4cccfd98b66d0f3520e9698b157da1e5de9
Kelsey <kelsey.pereiracosta@dawsoncollege.qc.ca>
2022-08-29 12:41:46 -0400

    started implementing interface methods

--- PianoSimulator/CircularArray.cs
+++ PianoSimulator/CircularArray.cs
@@ -6,20 +6,30 @@ namespace PianoSimulator
     public class CircularArray : IRingBuffer
     {
         private double[] _array;
-        private int _length;
+        // private int _length;
+        private int _frontOfArr;
 
         public CircularArray(int size)
         {
-            _length = size;
+            _array = new double[size];
+            // _length = 0;
+            _frontOfArr = 0;
         }
 
-        public double this[int index] => throw new NotImplementedException();
+        public double this[int index] => (_frontOfArr + index) % _array.Length;
 
-        public int Length => throw new NotImplementedException();
+        public int Length => _array.Length;
 
         public void Fill(double[] array)
         {
-            throw new NotImplementedException();
+            if (array.Length != _array.Length)
+            {
+                throw new IndexOutOfRangeException("Array lengths do not match");
+            }
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = array[i];
+            }
         }
 
         public double Shift(double value)
