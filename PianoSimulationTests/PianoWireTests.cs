69cf374aa4854dcb9834831f694839b382c2583a
Kelsey <kelsey.pereiracosta@dawsoncollege.qc.ca>
2022-09-01 11:08:30 -0400

    added misc files that werent commited properly

---
+++ PianoSimulationTests/PianoWireTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PianoSimulationTests
+{
+    [TestClass]
+    public class PianoWireTests{
+        [TestMethod]
+        public void testNoteFrequency(){
+
+        }
+        [TestMethod]
+        public void testSampleRate(){
+            
+        }
+        [TestMethod]
+        public void testSampler(){
+
+        }
+        [TestMethod]
+        public void testStrike(){
+
+        }
+    }   
+}
