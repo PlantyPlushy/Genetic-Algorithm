9e9da9556e18b6dd8a9c029cee7568bdf7e56e2b
Kelsey Pereira Costa <kelsey.pereiracosta@dawsoncollege.qc.ca>
2022-09-11 16:00:31 -0400

    finished beautifying console app

--- KeyboardPiano/Program.cs
+++ KeyboardPiano/Program.cs
@@ -11,20 +11,8 @@ namespace KeyboardPiano
         static void Main(string[] args)
         {
             Piano piano = new Piano("q2w3er5t6y7ui9o0p");
-            Audio audio = new Audio();
-            
-            // while (true)
-            // {
-            //     Console.ReadKey().KeyChar;
-            // }
-
-            // char userKey = Console.ReadKey().KeyChar;
-            // piano.StrikeKey(userKey);
-            // for (int i = 0; i < 44100 * 3; i++)
-            // {
-            //     audio.Play(piano.Play());
-            // }
-            
+            Audio audio = new Audio(4096);
+ 
             Console.Write(
                 " _______________________________________ \n" +
                 "|  | | | |  |  | | | | | |  |  | | | |  |\n" +
@@ -35,23 +23,21 @@ namespace KeyboardPiano
                 "| q | w | e | r | t | y | u | i | o | p |\n" +
                 "|___|___|___|___|___|___|___|___|___|___|\n");
             Console.WriteLine("Press any of the available keys above or ESC to exit");
-            // "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' "
+ 
             while (true)
             {
-                var key = Console.ReadKey(false);
-                if (key.Key == ConsoleKey.Escape)
+                // Console.Write( Console.KeyAvailable);
+                if (Console.KeyAvailable)
                 {
-                    break;
-                } 
-                else
-                {
+                    var key = Console.ReadKey(false);
+                    if (key.Key == ConsoleKey.Escape)
+                    {
+                        break;
+                    } 
                     char userKey = key.KeyChar;
                     piano.StrikeKey(userKey);
-                    for (int i = 0; i < 44100; i++)
-                    {
-                        audio.Play(piano.Play());
-                    }
                 }
+                audio.Play(piano.Play());
             }
         }
     }
