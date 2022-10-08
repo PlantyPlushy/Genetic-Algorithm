6c5b01b8aef23949c0c1ad5432d6381b9919ae35
Kelsey <kelsey.pereiracosta@dawsoncollege.qc.ca>
2022-10-08 16:59:09 -0400

    on press, keys now display their associated note

--- InteractivePiano/SpriteRender/Key.cs
+++ InteractivePiano/SpriteRender/Key.cs
@@ -9,9 +9,11 @@ namespace SpriteRender
     {
         private bool _isWhite;
         private string _note;
+        private bool _isPressed;
         private Texture2D _texture;
         private Color _color;
         private Vector2 _position;
+        private SpriteFont _noteTexture;
 
         public string Note 
         { 
@@ -25,19 +27,26 @@ namespace SpriteRender
             _color = Color.White;
             _position = position;
             _note = note;
+            _isPressed = false;
         }
         
         // When a key is pressed, change color of the key
         public void Press(){
             this._color = Color.Red;
+            this._isPressed = true;
         }
         public void UnPress(){
             this._color = Color.White;
+            this._isPressed = false;
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(_texture, _position, _color);
+            if(_isPressed)
+            {
+                spriteBatch.DrawString(_noteTexture, _note, new Vector2(_position.X, 100), Color.DarkBlue);
+            }
         }
 
 
@@ -49,8 +58,9 @@ namespace SpriteRender
             } else 
             {
             _texture = manager.Load<Texture2D>("blackKey");
-
             }
+            _noteTexture = manager.Load<SpriteFont>("KeyNote"); 
+
         }
     }
 }
